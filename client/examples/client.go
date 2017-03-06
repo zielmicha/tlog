39d910e35e5c814bf147f0e2deec4015534dd901
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-03-06 11:12:09 +0000

    tlog client example

---
+++ client/examples/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/g8os/tlog/client"
+)
+
+func main() {
+	var volID uint32 = 0x1f
+	num_conn := 2
+	clients := make([]*client.Client, num_conn)
+
+	for i := 0; i < num_conn; i++ {
+		client, err := client.New("127.0.0.1:11211")
+		if err != nil {
+			log.Fatal(err)
+		}
+		clients[i] = client
+	}
+	data := make([]byte, 4096*4)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 30; i++ {
+		wg.Add(1)
+		act := func(j int) {
+			defer wg.Done()
+			client := clients[j%num_conn]
+
+			err := client.Send(volID, uint32(j), uint32(j), uint64(j), data)
+			if err != nil {
+				log.Fatalf("failed to send :%v", err)
+			}
+
+		}
+		//go act(i)
+		act(i)
+	}
+	wg.Wait()
+}
