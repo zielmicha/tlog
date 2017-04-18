570903eec6969f5536fbc7d6214a5732d0af296e
Michał Zieliński <michal@zielinscy.org.pl>
2017-04-18 13:17:58 +0200

    nim: bench_client fix

--- nim/tlog/bench_client.nim
+++ nim/tlog/bench_client.nim
@@ -2,13 +2,13 @@ import reactor, tlog/util, strutils, capnp, tlog/schema, strutils, os
 
 proc main() {.async.} =
   let conn = await connectTcp("localhost", 11211)
-  let data = " ".repeat(1024 * 16)
+  var data = " ".repeat(1024 * 16)
   var dataMsg = packPointer(TlogBlock(
     volumeId: 2,
     sequence: 1,
     lba: 1,
     size: data.len.uint32,
-    data: data,
+    data: data.stringView,
     timestamp: 111))
 
   dataMsg.setLen(16472 - 8)
