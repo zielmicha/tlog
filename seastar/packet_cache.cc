7ed1e0d2dd1bbd6dc84d4ba73a696cc6f1a1f425
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-04-19 21:13:19 +0700

    seastar : use queue instead of map to hold tlog block.
    It was using map because it assumed that tlog message might come in
    unordered manner.

--- seastar/packet_cache.cc
+++ seastar/packet_cache.cc
@@ -1,14 +1,17 @@
 #include "packet_cache.h"
 
+void packet_cache::add(tlog_block *tb) {
+	_packets.push(tb);
+}
 bool packet_cache::pick(std::queue<tlog_block *> *q, int flush_size) {
 	if (this->size() == 0 || this->size() < flush_size) {
 		return false;
 	}
 	
-	auto it = _packets.begin();
-	for (int i=0; i  < flush_size && it != _packets.end(); i++) {
-		q->push(it->second);
-		it = _packets.erase(it);
+	for (int i=0; i  < flush_size && !_packets.empty(); i++) {
+		auto tb = _packets.front();
+		_packets.pop();
+		q->push(tb);
 	}
 	return true;
 }
--- seastar/packet_cache.h
+++ seastar/packet_cache.h
@@ -9,7 +9,7 @@
 
 class packet_cache {
 private:
-	std::map<uint64_t, tlog_block *> _packets;
+	std::queue<tlog_block *> _packets;
 public:
 	std::string _vol_id;
 	uint32_t _vol_id_number;
@@ -23,9 +23,7 @@ public:
 	int size() {
 		return _packets.size();
 	}
-	void add(tlog_block *tb) {
-		_packets[tb->_sequence] = tb;
-	}
+	void add(tlog_block *tb);
 	bool pick(std::queue<tlog_block *> *q, int flush_size);
 };
 
